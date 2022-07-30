6b5ae8d51ce213fb7965961224489f36c581899b
Edwin0204 <edwins0204@gmail.com>
2022-07-30 18:12:10 -0500

    get thoughts

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,4 +1,29 @@
 const { User, Thought } = require('../models');
 
+const thoughtController = {
+   
+    getAllThought(req, res) {
+        Thought.find({})
+            .select('-__v')
+            .sort({ createdAt: -1 })
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.thoughtId })
+            .select('-__v')
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+
+};
 
 module.exports = thoughtController;
